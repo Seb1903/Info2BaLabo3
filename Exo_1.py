d2e114ca0a12f329afdb32eb3153922bc5cac0f2
Seb1903 <sebastien.martinez99@gmail.com>
2020-03-05 09:30:02 +0100

    Add compute

--- Exo_1.py
+++ Exo_1.py
@@ -1,5 +1,5 @@
-def call(fonction) : 
-    return fonction()
+def call(fonction, *args) : 
+    return fonction(*args)
 
 
 def hello(): 
@@ -10,3 +10,14 @@ def add(a,b) :
     return a + b
 
 print(call(add, 2, 9))   
+
+def sub(a,b)  :
+    return a - b 
+
+def compute(a, b, op = add) :  #Ici on crée un paramètre optionnel op en lui donnant une valeur (par défaut) 
+    if op == add : 
+        return add(a,b)
+    if op == sub : 
+        return sub(a,b)
+
+print(compute(12,3, op=sub))
